0d2e9ede0579e195a3e1758cee2f3727b521d40f
Phonglaai <tranhoangphong02082000@gmail.com>
2025-08-28 15:30:32 +0700

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,13 @@
+public class Book {
+    String title;
+    String author;
+    double price;
+
+    public Book() {}
+
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+}
---
+++ BookApp.java
@@ -0,0 +1,9 @@
+public class BookApp {
+    public static void main(String[] args) {
+        Book book1 = new Book("Dac Nhan Tam", "Dale Carnegie", 86.000);
+        Book book2 = new Book("Cay cam ngot cua toi", "Jose Mauro de Vasconcelos", 75.000);
+
+        System.out.println("Book 1: " + book1.title + " by " + book1.author + " - đ" + book1.price);
+        System.out.println("Book 2: " + book2.title + " by " + book2.author + " - đ" + book2.price);
+    }
+}
