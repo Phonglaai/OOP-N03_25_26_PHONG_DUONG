96266d8b84d54485519f16ef2623f8e604f14d63
Phonglaai <tranhoangphong02082000@gmail.com>
2025-10-09 15:25:17 +0700

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,13 +1,1 @@
-public class Book {
-    String title;
-    String author;
-    double price;
 
-    public Book() {}
-
-    public Book(String title, String author, double price) {
-        this.title = title;
-        this.author = author;
-        this.price = price;
-    }
-}
