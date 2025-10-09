1250c1934c31621099f2ea8f6d9cd2de069a7931
Phonglaai <tranhoangphong02082000@gmail.com>
2025-10-09 15:25:32 +0700

    Update Baitap.java

--- Baitap.java
+++ Baitap.java
@@ -1,18 +1,1 @@
-public class Bia {
-    String nhanbia;
-    String tenbia;
-    String mausac;
-    int luuluong;
 
-    public String uong(String tenbia){
-        System.out.println("Bia::" + tenbia);
-        return tenbia;
-
-    }
-    public String getMausac(){
-        return mausac;
-    }
-    public String getTen(){
-        return tenbia;
-    }
-}
